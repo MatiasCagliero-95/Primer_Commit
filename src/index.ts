a87c522e026c28c13270d60ff4ca151beb92e524
Matias Cagliero <93091391+MatiasCagliero-95@users.noreply.github.com>
2022-08-03 21:58:42 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hola Mati!</h1>
+<div>
+Qué capo, ya estoy usando github y voy hacer
+mi primer commit <p> Hola mamá, ando codeando </p>
+  
+</div>`;
